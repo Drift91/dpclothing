9f2deac8da77c136de8dc0fa14f78375b1daaa73
Austin Conway <77038583+Collaze@users.noreply.github.com>
2023-05-16 07:20:03 -0500

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ game 'gta5'
 
 author 'dullpear (Forked by Collaze)'
 description 'dpClothing++'
-version '1.0.5'
+version '1.0.6'
 
 client_scripts {
 	'client/functions.lua', 		-- Global Functions / Events / Debug and Locale start.
